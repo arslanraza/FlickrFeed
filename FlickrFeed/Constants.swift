52b514fc290c59ff08c454aec2b8141d197518a8
Arslan Raza <arslan_raza21@hotmail.com>
2016-04-30 15:13:30 +0400

    - Adds FeedViewController class - Adds Constants class to hold all the app constants - Removes ViewController class and replaces it with FeedViewController in Main.storyboard

---
+++ FlickrFeed/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  FlickrFeed
+//
+//  Created by Arslan Raza on 30/04/2016.
+//  Copyright © 2016 Arslan Raza. All rights reserved.
+//
+
+// =================================
+// This file will keep the constants that are required throughout the app
+// =================================
+
+import Foundation
---
+++ FlickrFeed/Controllers/FeedViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FeedViewController.swift
+//  FlickrFeed
+//
+//  Created by Arslan Raza on 30/04/2016.
+//  Copyright © 2016 Arslan Raza. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    
+    // MARK: Private methods
+    
+    // MARK: Life Cycle methods
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: Public methods
+
+}
